f3b45e90cb16c81830107eccd09ab28e3faf361b
jmartinrivera11 <84155977+jmartinrivera11@users.noreply.github.com>
2024-08-02 16:37:43 -0600

    Interafce Cuadriculas

---
+++ src/main/java/com/mycompany/laboratorio3/Cuadricula.java
@@ -0,0 +1,7 @@
+
+package com.mycompany.laboratorio3;
+
+public interface Cuadricula {
+    public boolean numero3x3(int row, int col);
+    public boolean numero3x3Inicial(int row, int col, int num);
+}
